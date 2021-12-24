c24880da038715d19669619261e631c4dfff362d
Yury Verenich <verenichyi@gmail.com>
2021-12-24 12:16:04 +0300

    feat: add input

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {ChangeEvent, FormEvent, useState} from 'react';
+
+import styles from './todo-list.module.scss';
 
 const TodoList = () => {
-    return (
-        <></>
-    )
+	const [value, setValue] = useState('');
+
+	const handleSubmit = (event: FormEvent): void => {
+		event.preventDefault();
+
+	}
+
+	const handleChange = (event: ChangeEvent<HTMLInputElement>): void => setValue(event.target.value);
+
+	return (
+		<div className={styles.list}>
+			<form onSubmit={handleSubmit}>
+				<input value={value} onChange={handleChange} className={styles.addTodoInput} placeholder={'Add todo...'} type="text"/>
+			</form>
+		</div>
+	)
 };
 
 export default TodoList;
