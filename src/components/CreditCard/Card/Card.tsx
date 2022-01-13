ce0551678a101b0b5e63f7734af2a037aa20c6ca
verenichyi <verenichyi@gmail.com>
2022-01-13 11:50:51 +0300

    refactor: change card appearance

--- src/components/CreditCard/Card/Card.tsx
+++ src/components/CreditCard/Card/Card.tsx
@@ -1,19 +1,78 @@
-import React from "react";
-import {RootStateOrAny, useSelector} from "react-redux";
+import React, {ChangeEvent, useMemo} from "react";
+import {RootStateOrAny, useDispatch, useSelector} from "react-redux";
 
 import styles from "./Card.module.scss";
 import sticker from 'src/assets/card/chip.png';
-import CardItem from "./CardItem/CardItem";
+import Cleave from "cleave.js/react";
+import Dropdown from "../../Dropdown/Dropdown";
+import {
+	setCardHolder,
+	setCardType,
+	setCardTypeImage,
+	setCreditCardNum,
+	setCVV,
+	setExpireMonth, setExpireYear
+} from "../../../redux/actionCreators";
+import {expMonths, expYears, imageUrls} from "../../../constants/creditCard";
 
 const Card = () => {
+	const dispatch = useDispatch();
+
 	const {
-		creditCardNum,
+		cardType,
 		cardTypeImage,
 		cardHolder,
-		cvv,
+		creditCardNum,
 		expireMonth,
-		expireYear
+		expireYear,
+		cvv
 	} = useSelector((state: RootStateOrAny) => state.cardReducer);
+
+	const handleType = (type: string): void => {
+		dispatch(setCardType(type));
+
+		const cardType = Object.keys(imageUrls).find((item) => item === type);
+
+		if (cardType) dispatch(setCardTypeImage(imageUrls[type]));
+	};
+
+	const handleFormItem = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>): void => {
+		const value = event.target.value;
+
+		switch (event.target.name) {
+			case 'cvv':
+				dispatch(setCVV(value));
+				if (!value.trim()) {
+					dispatch(setCVV('***'));
+				}
+				break;
+
+			case 'cardholder':
+				dispatch(setCardHolder(value));
+				break;
+
+			case 'number':
+				dispatch(setCreditCardNum(value));
+				if (!value.trim()) {
+					dispatch(setCreditCardNum('**** **** **** ****'));
+				}
+				break;
+		}
+	};
+
+	const handleDropdown = (name: string, value: number): void => {
+		switch (name) {
+			case 'month':
+				dispatch(setExpireMonth(value));
+				break;
+
+			case 'year':
+				dispatch(setExpireYear(value))
+		}
+	};
+
+	const expYearsItems = useMemo(() => expYears, [expYears]);
+	const expMonthsItems = useMemo(() => expMonths, [expMonths]);
 
 	return (
 		<div className={styles.card}>
@@ -21,14 +80,31 @@ const Card = () => {
 				<img className={styles.sticker} src={sticker} alt="Sticker"/>
 				<img className={styles.logo} src={cardTypeImage} alt="Card logo"/>
 			</div>
-
-			<CardItem title={'Card Number'} value={creditCardNum}/>
+				<Cleave
+					className={styles.input}
+					options={{
+						creditCard: true,
+						onCreditCardTypeChanged: handleType,
+						delimiter: ' '
+					}}
+					name={'number'}
+					value={creditCardNum}
+					onChange={handleFormItem}
+					placeholder="Card number"
+				/>
+				<input value={cardHolder} onChange={handleFormItem} className={styles.input} name={'cardholder'} type="text"
+							 placeholder="Card holder" required/>
 
 			<div className={styles.details}>
-				<CardItem title={'Card Holder'} value={cardHolder}/>
-				<CardItem title={'Expiration'} value={`${expireMonth} / ${expireYear}`}/>
-				<CardItem title={'CVV'} value={cvv}/>
+					<div className={styles.expiration}>
+						<Dropdown name={'month'} selected={expireMonth} setSelected={handleDropdown} options={expMonthsItems}/>
+						<Dropdown name={'year'} selected={expireYear} setSelected={handleDropdown} options={expYearsItems}/>
+					</div>
+					<input value={cvv} onChange={handleFormItem} maxLength={3} type="password" name={'cvv'} placeholder="CVV"
+								 className={`${styles.input} ${styles.cvv}`}
+								 required/>
 			</div>
+
 		</div>
 	);
 };
--- src/components/CreditCard/CreditCard.tsx
+++ src/components/CreditCard/CreditCard.tsx
@@ -1,73 +1,13 @@
-import React, {ChangeEvent, FormEvent, useMemo, useState} from 'react';
-import Cleave from 'cleave.js/react';
-import {RootStateOrAny, useDispatch, useSelector} from 'react-redux';
+import React, {FormEvent} from 'react';
+import {RootStateOrAny, useSelector} from 'react-redux';
 
 import styles from './CreditCard.module.scss';
-import {expMonths, expYears, imageUrls} from 'src/constants/creditCard';
 
-import {
-	setCardHolder,
-	setCardType,
-	setCardTypeImage,
-	setCreditCardNum,
-	setCVV,
-	setExpireMonth, setExpireYear
-} from 'src/redux/actionCreators';
 import Card from './Card/Card';
 import {encryptData} from '../../helpers/card';
-import Dropdown from '../Dropdown/Dropdown';
 
 const CreditCard = () => {
-	// const [selectedMonth, setSelectedMonth] = useState(1);
-	// const [selectedYear, setSelectedYear] = useState(2022);
-
-	const dispatch = useDispatch();
-
-	const {cardType,cardHolder,creditCardNum, expireMonth, expireYear, cvv} = useSelector((state: RootStateOrAny) => state.cardReducer);
-
-
-	const handleType = (type: string): void => {
-		dispatch(setCardType(type));
-
-		const cardType = Object.keys(imageUrls).find((item) => item === type);
-
-		if (cardType) dispatch(setCardTypeImage(imageUrls[type]));
-	};
-
-	const handleFormItem = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>): void => {
-		const value = event.target.value;
-
-		switch (event.target.name) {
-			case 'cvv':
-				dispatch(setCVV(value));
-				if (!value.trim()) {
-					dispatch(setCVV('***'));
-				}
-				break;
-
-			case 'cardholder':
-				dispatch(setCardHolder(value));
-				break;
-
-			case 'number':
-				dispatch(setCreditCardNum(value));
-				if (!value.trim()) {
-					dispatch(setCreditCardNum('**** **** **** ****'));
-				}
-				break;
-		}
-	};
-
-	const handleDropdown = (name: string, value: number): void => {
-		switch (name) {
-			case 'month':
-				dispatch(setExpireMonth(value));
-				break;
-
-			case 'year':
-				dispatch(setExpireYear(value))
-		}
-	};
+	const {cvv, cardType} = useSelector((state: RootStateOrAny) => state.cardReducer);
 
 	const handleSubmit = (event: FormEvent) => {
 		event.preventDefault();
@@ -76,53 +16,10 @@ const CreditCard = () => {
 		console.log(hashCVV)
 	};
 
-	const expYearsItems = useMemo(() => expYears, [expYears]);
-	const expMonthsItems = useMemo(() => expMonths, [expMonths]);
-
 	return (
 		<>
 			<form onSubmit={handleSubmit} className={styles.form}>
 				<Card/>
-
-				<div className={styles.inputContainer}>
-					<h4>Enter card number</h4>
-					<Cleave
-						className={styles.input}
-						options={{
-							creditCard: true,
-							onCreditCardTypeChanged: handleType,
-							delimiter: ' '
-						}}
-						name={'number'}
-						value={creditCardNum}
-						onChange={handleFormItem}
-						placeholder="Please enter your credit card number"
-					/>
-				</div>
-				<div className={styles.inputContainer}>
-					<h4>Card Holder</h4>
-					<input value={cardHolder} onChange={handleFormItem} className={styles.input} name={'cardholder'} type="text"
-								 placeholder="Please enter your full name" required/>
-				</div>
-
-				<div className={styles.details}>
-					<div className={styles.inputContainer}>
-						<h4>Exp Month</h4>
-						<Dropdown name={'month'} selected={expireMonth} setSelected={handleDropdown} options={expMonthsItems}/>
-					</div>
-					<div className={styles.inputContainer}>
-						<h4>Exp Year</h4>
-						<Dropdown name={'year'} selected={expireYear} setSelected={handleDropdown} options={expYearsItems}/>
-					</div>
-
-					<div className={styles.inputContainer}>
-						<h4>CVV</h4>
-						<input value={cvv} onChange={handleFormItem} maxLength={3} type="password" name={'cvv'} placeholder="CVV"
-									 className={styles.input}
-									 required/>
-					</div>
-				</div>
-
 				<button className={styles.button}>{`Submit ${cardType} payment`}</button>
 			</form>
 		</>
